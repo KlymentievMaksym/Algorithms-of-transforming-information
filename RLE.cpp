cd943502af38882b3067ec39e703b9c8045a919e
Dadmon <maksklym-ipt27@lll.kpi.ua>
2025-02-15 16:28:11 +0200

    Base RLE encoding and decoding

--- RLE.cpp
+++ RLE.cpp
@@ -34,13 +34,23 @@ class RLE
             string s;
             while (getline(fr, s))
             {
-                // cout << s << s[1] << size(s) << bitset<8>(s[1]).to_string() << "\n";
+                int count = 1;
                 for (int i = 0; i < size(s); i++)
                 {
+                    while (i < size(s) - 1 and s[i] == s[i+1])
+                    {
+                        count++;
+                        i++;
+                    }
+                    // cout << count << "\n";
+                    // cout << bitset<1>(1) << bitset<7>(count) << "\n";
+                    // cout << s[i] << "\n";
+                    // cout << bitset<8>(s[i]) << "\n" << "\n";
+                    fw << bitset<1>(1) << bitset<7>(count);
                     fw << bitset<8>(s[i]);
-                    // cout << i << "\n";
-                    // cout << bitset<8>(s[i]) << "\n";
+                    count = 1;
                 }
+                fw << bitset<1>(1) << bitset<7>(count);
                 fw << bitset<8>('\n');
             }   
             fr.close();
@@ -53,15 +63,19 @@ class RLE
             if (filename_write == "")
             {
                 filename_write = filename_read;
-                filename_write.pop_back();
-                filename_write.pop_back();
-                filename_write.pop_back();
-                filename_write.pop_back();
-                // cout << filename_write << "\n";
-                // auto it = next(filename_read.begin(), filename_read.find(".rle"));
-                // filename_write = filename_read;
-                // filename_write.erase(it, next(it, 2));
-                // filename_write = filename_write + "_COPY.txt";
+                if (filename_write.find(".rle") != string::npos)
+                {
+                    filename_write.pop_back();
+                    filename_write.pop_back();
+                    filename_write.pop_back();
+                    filename_write.pop_back();
+                }
+                else
+                {
+                    filename_write = filename_write + "_copy.txt";
+                }
+                // cout << filename_write << '\n';
+                // cout << filename_read << '\n';
             }
 
             ifstream fr(filename_read, ios::binary);
@@ -80,20 +94,55 @@ class RLE
 
             vector<unsigned char> buffer(istreambuf_iterator<char>(fr), {});
 
+            unsigned char character;
+            unsigned long integer = 0;
+            string text = "";
             for (int j = 0; j < size(buffer); j = j + 8)
             {
                 vector<unsigned char> slice(buffer.begin() + j, buffer.begin() + j + 8);
                 string s = "";
-                for (auto it : slice)
+                if (slice[0] == '1')
                 {
-                    s = s + static_cast<char>(it);
+                    for (unsigned long k = 0; k < integer; k++)
+                    {
+                        // cout << text;
+                        fw << text;
+                    }
+                    integer = 0;
+                    text = "";
+                    for (int i = 1; i < size(slice); i++)
+                    {
+                        s = s + static_cast<char>(slice[i]);
+                    }
+                    bitset<7> byt;
+                    byt = static_cast<bitset<7>>(s);
+                    integer = byt.to_ulong();
                 }
-                bitset<8> byt;
-                byt = static_cast<bitset<8>>(s);
-                unsigned long i = byt.to_ulong(); 
-                unsigned char c = static_cast<unsigned char>( i );
-                fw << c;
-                // cout << c;
+                else
+                {
+                    if (slice[0] == '0')
+                    {
+                        for (auto it : slice)
+                        {
+                            s = s + static_cast<char>(it);
+                        }
+                        bitset<8> byt;
+                        byt = static_cast<bitset<8>>(s);
+                        unsigned long i = byt.to_ulong(); 
+                        character = static_cast<unsigned char>( i );
+                        text = text + static_cast<char>(character);
+                    }
+                }
+            }
+            if (integer != 0)
+            {
+                for (unsigned long k = 0; k < integer; k++)
+                {
+                    // cout << text;
+                    fw << text;
+                }
+                integer = 0;
+                text = "";
             }
             
 
@@ -112,18 +161,18 @@ int main(int argc, char* argv[])
     // cout << "You have entered " << argc
     //      << " arguments:" << endl;
 
-    // for (int i = 0; i < argc; i++)
-    // {
-    //     cout << "Argument " << i + 1 << ": " << argv[i]
-    //          << endl;
-    // }
+    for (int i = 1; i < argc; i++)
+    {
+        cout << "File " << i << ": " << argv[i]
+             << endl;
+    }
 
     while (true)
     {
-        cout << "[?] What to do with it?\n1: Encode, 2: Decode, 0: Exit\n>>> ";
+        cout << "\n[?] What to do with it?\n1: Encode, 2: Decode, 0: Exit\n>>> ";
         string ask;
         cin >> ask;
-        cout << ask << "\n";
+        // cout << ask << "\n";
 
         if (ask == "0")
         {
