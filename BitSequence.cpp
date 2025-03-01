300a19546f03e697c56a1feb0e2bb3e18e66c9f1
Dadmon <maksklym-ipt27@lll.kpi.ua>
2025-03-01 20:48:17 +0200

    PROGRESS???

--- BitSequence.cpp
+++ BitSequence.cpp
@@ -14,7 +14,44 @@ class BitSequence
 {
     public:
         // ReadBitSequence(BitStream, b1, 11),
-        int FINISHED_BITS = 0;
+        int FINISHED_BITS_READ = 0;
+        int FINISHED_BITS_WRITE = 0;
+
+        unsigned char reverse_table[256] = {
+            0x00, 0x80, 0x40, 0xc0, 0x20, 0xa0, 0x60, 0xe0,
+            0x10, 0x90, 0x50, 0xd0, 0x30, 0xb0, 0x70, 0xf0,
+            0x08, 0x88, 0x48, 0xc8, 0x28, 0xa8, 0x68, 0xe8,
+            0x18, 0x98, 0x58, 0xd8, 0x38, 0xb8, 0x78, 0xf8,
+            0x04, 0x84, 0x44, 0xc4, 0x24, 0xa4, 0x64, 0xe4,
+            0x14, 0x94, 0x54, 0xd4, 0x34, 0xb4, 0x74, 0xf4,
+            0x0c, 0x8c, 0x4c, 0xcc, 0x2c, 0xac, 0x6c, 0xec,
+            0x1c, 0x9c, 0x5c, 0xdc, 0x3c, 0xbc, 0x7c, 0xfc,
+            0x02, 0x82, 0x42, 0xc2, 0x22, 0xa2, 0x62, 0xe2,
+            0x12, 0x92, 0x52, 0xd2, 0x32, 0xb2, 0x72, 0xf2,
+            0x0a, 0x8a, 0x4a, 0xca, 0x2a, 0xaa, 0x6a, 0xea,
+            0x1a, 0x9a, 0x5a, 0xda, 0x3a, 0xba, 0x7a, 0xfa,
+            0x06, 0x86, 0x46, 0xc6, 0x26, 0xa6, 0x66, 0xe6,
+            0x16, 0x96, 0x56, 0xd6, 0x36, 0xb6, 0x76, 0xf6,
+            0x0e, 0x8e, 0x4e, 0xce, 0x2e, 0xae, 0x6e, 0xee,
+            0x1e, 0x9e, 0x5e, 0xde, 0x3e, 0xbe, 0x7e, 0xfe,
+            0x01, 0x81, 0x41, 0xc1, 0x21, 0xa1, 0x61, 0xe1,
+            0x11, 0x91, 0x51, 0xd1, 0x31, 0xb1, 0x71, 0xf1,
+            0x09, 0x89, 0x49, 0xc9, 0x29, 0xa9, 0x69, 0xe9,
+            0x19, 0x99, 0x59, 0xd9, 0x39, 0xb9, 0x79, 0xf9,
+            0x05, 0x85, 0x45, 0xc5, 0x25, 0xa5, 0x65, 0xe5,
+            0x15, 0x95, 0x55, 0xd5, 0x35, 0xb5, 0x75, 0xf5,
+            0x0d, 0x8d, 0x4d, 0xcd, 0x2d, 0xad, 0x6d, 0xed,
+            0x1d, 0x9d, 0x5d, 0xdd, 0x3d, 0xbd, 0x7d, 0xfd,
+            0x03, 0x83, 0x43, 0xc3, 0x23, 0xa3, 0x63, 0xe3,
+            0x13, 0x93, 0x53, 0xd3, 0x33, 0xb3, 0x73, 0xf3,
+            0x0b, 0x8b, 0x4b, 0xcb, 0x2b, 0xab, 0x6b, 0xeb,
+            0x1b, 0x9b, 0x5b, 0xdb, 0x3b, 0xbb, 0x7b, 0xfb,
+            0x07, 0x87, 0x47, 0xc7, 0x27, 0xa7, 0x67, 0xe7,
+            0x17, 0x97, 0x57, 0xd7, 0x37, 0xb7, 0x77, 0xf7,
+            0x0f, 0x8f, 0x4f, 0xcf, 0x2f, 0xaf, 0x6f, 0xef,
+            0x1f, 0x9f, 0x5f, 0xdf, 0x3f, 0xbf, 0x7f, 0xff,
+        };
+
         template<size_t N>
         void Reverse(bitset<N> &bitst)
         {
@@ -25,18 +62,18 @@ class BitSequence
                 bitst[N-i-1] = t;
             }
         }
-        int Read(fstream &bitsream, vector<char> &Arr, int BitLength)
+        int Read(fstream &bitstream, vector<uint8_t> &Arr, int BitLength)
         {
             uint8_t value;
             bitset<8> val;
-            bitsream.seekg(FINISHED_BITS/8);
-            cout << "Cursor in file on " << FINISHED_BITS << " bits (" << bitsream.tellg() << " byte)\n";
-            while (!bitsream.eof( ) and bitsream.tellg()*8 - FINISHED_BITS < BitLength)
+            bitstream.seekg(FINISHED_BITS_READ/8);
+            cout << "Cursor in file on " << FINISHED_BITS_READ << " bits (" << bitstream.tellg() << " byte)\n";
+            while (!bitstream.eof( ) and bitstream.tellg()*8 - FINISHED_BITS_READ < BitLength)
             {
-                bitsream.read(reinterpret_cast<char*>(&value), sizeof(value));
-                if (bitsream.tellg()*8 - FINISHED_BITS >= BitLength)
+                bitstream.read(reinterpret_cast<char*>(&value), sizeof(value));
+                if (bitstream.tellg()*8 - FINISHED_BITS_READ >= BitLength)
                 {
-                    int bits_left = bitsream.tellg()*8 - FINISHED_BITS - BitLength;
+                    int bits_left = bitstream.tellg()*8 - FINISHED_BITS_READ - BitLength;
                     int bits_to_use = 8 - bits_left;
                     
                     // string str = bitset<8>(value).to_string();
@@ -54,7 +91,7 @@ class BitSequence
                     // cout << res << " | " << bitset<8>(res) << " | " << (bitset<8>(res) << bits_left) << " | " << ((bitset<8>(res) << bits_left) & val) << " | " << '\n';
                     bitset<8> result = (bitset<8>(res) << bits_left) & val;
 
-                    // cout << bitsream.tellg()*8 << " | " << value << " | " << bitset<8>(value) << " | " << val << " | " << result << '\n';
+                    // cout << bitstream.tellg()*8 << " | " << value << " | " << bitset<8>(value) << " | " << val << " | " << result << '\n';
                 }
                 else
                 {
@@ -62,24 +99,50 @@ class BitSequence
                     // Reverse(val);
                 }
                 // cout << bitset<8>(value) << ' ';
-                Arr.push_back(static_cast<char>(val.to_ulong()));
+                Arr.push_back(static_cast<uint8_t>(val.to_ulong()));
                 // cout << static_cast<char>(value);
             }
-            FINISHED_BITS += BitLength;
+            FINISHED_BITS_READ += BitLength;
             return 0;
         }
         // WriteBitSequence(BitStream, a1, 9),
-        int Write(fstream &bitsream, vector<char> &Arr, int BitLength)
+        int Write(fstream &bitstream, vector<uint8_t> &Arr, int BitLength)
         {
             uint8_t value;
-            bitset<8> val;
-            bitsream.seekg(FINISHED_BITS/8);
-            cout << "Cursor in file on " << FINISHED_BITS << " bits (" << bitsream.tellg() << " byte)\n";
-            for (auto it : Arr)
+            // bitset<8> val;
+            // bitstream.seekg(FINISHED_BITS_READ/8);
+            // cout << "Cursor in file on " << FINISHED_BITS_READ << " bits (" << bitstream.tellg() << " byte)\n";
+            for (uint8_t it : Arr)
             {
-                bitsream.write(reinterpret_cast<char*>(&it), sizeof(value));
+                // if (bitstream.tellg()*8 <= BitLength - (BitLength % 8))
+                // cout << '\n' << FINISHED_BITS_WRITE << '\n';
+                if (FINISHED_BITS_WRITE <= BitLength - (BitLength % 8))
+                {
+                    bitstream.write(reinterpret_cast<char*>(&it), sizeof(it));
+                    FINISHED_BITS_WRITE += sizeof(it)*8;
+                }
+                else
+                {
+                    // cout << bitset<8>(it & (BitLength % 8 << (8 - BitLength % 8))) << "\n\n\n";
+                    value = it & (BitLength % 8 << (8 - BitLength % 8));
+                    bitstream.write(reinterpret_cast<char*>(&value), sizeof(value));
+                    // val = 
+                }
+                // bitstream.write(reinterpret_cast<char*>(&it), sizeof(it));
             }
             return 0;
+        }
+    // private:
+        // unsigned char reverse(unsigned char b)
+        // {
+        //     b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
+        //     b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
+        //     b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
+        //     return b;
+        // }
+        unsigned char reverse(unsigned char x)
+        {
+            return reverse_table[x];
         }
 };
 
@@ -87,35 +150,34 @@ class BitSequence
 int main(int argc, char* argv[])
 {
     BitSequence bs;
-    vector<char> arr;
     string str = "test.txt";
-    int BitLength = 0;
+    // int BitLength = 0;
 
-    try
-    {
-        BitLength = stoi(argv[1]);
-    }
-    catch(const std::exception& e)
-    {
-        cerr << e.what() << '\n';
-        try
-        {
-            str = argv[1];
-        }
-        catch(const std::exception& e)
-        {
-            cerr << e.what() << '\n';
-        }
-        try
-        {
-            BitLength = stoi(argv[2]);
-        }
-        catch(const std::exception& e)
-        {
-            cerr << e.what() << '\n';
-        }
-    }
-    cout << "\n[T] DONE TRY\n";
+    // try
+    // {
+    //     BitLength = stoi(argv[1]);
+    // }
+    // catch(const std::exception& e)
+    // {
+    //     cerr << e.what() << '\n';
+    //     try
+    //     {
+    //         str = argv[1];
+    //     }
+    //     catch(const std::exception& e)
+    //     {
+    //         cerr << e.what() << '\n';
+    //     }
+    //     try
+    //     {
+    //         BitLength = stoi(argv[2]);
+    //     }
+    //     catch(const std::exception& e)
+    //     {
+    //         cerr << e.what() << '\n';
+    //     }
+    // }
+    // cout << "\n[T] DONE TRY\n";
 
     fstream fr(str, ios::binary | ios::in | ios::out);
     if (!fr.is_open())
@@ -127,18 +189,39 @@ int main(int argc, char* argv[])
     // bs.Read(fr, arr, 11);
     // bs.Read(fr, arr, 7);
     // bs.Read(fr, arr, 0);
-    arr.push_back(0b11100001);
-    bs.Write(fr, arr, 8);
-    bs.Read(fr, arr, 8);
+    // arr.push_back(0b11100001);
+    vector<uint8_t> a1 = {0b11100001, 0b00000001};
+    vector<uint8_t> a2 = {0b11101110, 0b00000000};
+    bs.Write(fr, a1, 9);
+    bs.Write(fr, a2, 9);
+    vector<uint8_t> b1, b2;
+    bs.Read(fr, b1, 11);
+    bs.Read(fr, b2, 7);
     fr.close();
-    // cout << std::bitset<4>(11)[1] << "\n";
-    for (auto it : arr)
+
+    for (uint8_t byte : b1)
     {
-        cout << bitset<8>(it) << ' ';
+        // cout << hex << static_cast<int>(byte) << " ";
+        cout << bitset<8>(byte) << " ";
     }
+    cout << dec << endl;
+    for (uint8_t byte : b2)
+    {
+        // cout << hex << static_cast<int>(byte) << " ";
+        cout << bitset<8>(byte) << " ";
+    }
+    cout << dec << endl;
+
+    // cout << bitset<8>(1) << " | " << bitset<8>(bs.reverse(1));
+
+    // cout << std::bitset<4>(11)[1] << "\n";
+    // for (auto it : arr)
+    // {
+    //     cout << bitset<8>(it) << ' ';
+    // }
     
     // string s;
     // cin >> s;
-    cout << "\n[T] DONE Main\n";
+    // cout << "\n[T] DONE Main\n";
     return 0;
 }
